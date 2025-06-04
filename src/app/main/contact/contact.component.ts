3b285c43c37d5fee0aca22cb2b0e8a2f3005bb67
Markus Fischer <fischer@mideasdesign.de>
2025-06-04 17:16:56 +0200

    add logic to the contactform and provideHttpClient to app.config

---
+++ src/app/main/contact/contact.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+http = inject(HttpClient);
+  contactData ={
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+    policy: ''
+  };
+ mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
+}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ProjectsComponent, ContactComponent, FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
