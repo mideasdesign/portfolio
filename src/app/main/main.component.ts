2e29338cd768545283b51a189ecc6e87694c8b41
Markus Fischer <fischer@mideasdesign.de>
2025-06-11 16:19:31 +0200

    mayior changes to all section for the mobile view and some refactoring tho the html

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,11 +4,12 @@ import { ContactComponent } from './contact/contact.component';
 import { FormsModule } from '@angular/forms';
 import {TranslateModule} from "@ngx-translate/core";
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ProjectsComponent, ContactComponent, TestimonialsComponent, FormsModule, TranslateModule],
+  imports: [SkillsComponent, ProjectsComponent, ContactComponent, TestimonialsComponent, FormsModule, TranslateModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
