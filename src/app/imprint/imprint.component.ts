c8f531a4fb057235c111b1cf6006ed6dafe6bff5
Markus Fischer <fischer@mideasdesign.de>
2025-07-29 18:23:59 +0200

    refactor: update the privacy and imprint page withe the new navbar

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
-import { HeaderComponent } from '../shared/header/header.component';
+import { NavComponent } from '../shared/nav/nav.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [CommonModule, TranslatePipe, HeaderComponent, FooterComponent],
+  imports: [CommonModule, TranslatePipe, NavComponent, FooterComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.css'
 })
--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
-import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { NavComponent } from '../shared/nav/nav.component';
 
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [CommonModule, TranslatePipe, HeaderComponent, FooterComponent],
+  imports: [CommonModule, TranslatePipe, NavComponent, FooterComponent],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.css'
 })
