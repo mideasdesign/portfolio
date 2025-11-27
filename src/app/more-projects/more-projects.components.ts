a3263865df35d3cf7e5625e7bffd136f1a965107
Markus Fischer <hello@mideasdesign.de>
2025-11-27 12:14:36 +0100

    I made the fields “process” and “team” optional so that they are only displayed if there is content.

--- src/app/more-projects/more-projects.components.ts
+++ src/app/more-projects/more-projects.components.ts
@@ -13,13 +13,13 @@ interface Project {
   titleKey: string;
   aboutTitleKey: string;
   aboutTextKey: string;
-  processTitleKey: string;
-  processTextKey: string;
+  processTitleKey?: string;
+  processTextKey?: string;
   teamTitleKey?: string;
   teamTextKey?: string;
   image: string;
-  liveUrl: string;
-  githubUrl: string;
+  liveUrl?: string;
+  githubUrl?: string;
 }
 
 @Component({
